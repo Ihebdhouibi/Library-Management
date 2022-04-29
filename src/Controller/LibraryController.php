6e2efe0780cfb70fa75ed9cbac5d83aad65ea946
iheb dhouibi <ihebdhouibi@polytechnicien.tn>
2022-04-29 02:52:05 +0100

    feature/VisitorAccess Home route added

---
+++ src/Controller/LibraryController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LibraryController extends AbstractController
+{
+    #[Route('/library', name: 'app_library')]
+    public function index(): Response
+    {
+        return $this->render('library/index.html.twig', [
+            'controller_name' => 'LibraryController',
+        ]);
+    }
+
+    #[Route('/', name: 'home')]
+    public function home() {
+        return $this->render('library/home.html.twig');
+    }
+}
