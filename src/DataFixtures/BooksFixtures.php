989f28651a69c96ad69564003dc161dedcfc14bd
iheb dhouibi <ihebdhouibi@polytechnicien.tn>
2022-04-29 03:45:04 +0100

    feature/VisitorAccess Dummy books added

--- src/DataFixtures/BooksFixtures.php
+++ src/DataFixtures/BooksFixtures.php
@@ -4,13 +4,21 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\Book;
 
 class BooksFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+
+        for($i=0; $i < 10; $i++){
+            $book = new Book();
+            $book->setTitle("Book title n $i");
+            $book->setAuthor("Book author $i");
+            $book->setContent("Book content $i");
+            $book->setStatus("disponible");
+            $manager->persist($book);
+        }
 
         $manager->flush();
     }
