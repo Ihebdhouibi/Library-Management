f1164327abd6393bf4faaab73334e5fdc177eace
iheb dhouibi <ihebdhouibi@polytechnicien.tn>
2022-04-29 04:09:52 +0100

    feature/VisitorAccess Visitor consult book done

--- src/Controller/LibraryController.php
+++ src/Controller/LibraryController.php
@@ -5,14 +5,28 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use App\Entity\Book;
 class LibraryController extends AbstractController
 {
     #[Route('/library', name: 'app_library')]
     public function index(): Response
     {
+        $repo = $this->getDoctrine()->getRepository(Book::class);
+        $books = $repo->findAll();
         return $this->render('library/index.html.twig', [
             'controller_name' => 'LibraryController',
+            'books' => $books
+        ]);
+    }
+
+    #[Route('/library/{id}', name: 'show_book')]
+    public function show($id){
+        $repo = $this->getDoctrine()->getRepository(Book::class);
+        $book = $repo->find($id);
+
+        return $this->render('library/show.html.twig', 
+        [
+            'book' => $book
         ]);
     }
 
